03564f54ecf264329b95faf143fa251f5a72ce3e
Marion Dutu <marion.dutulaunay@heig-vd.ch>
2019-03-10 18:19:32 +0100

    FileTransformerTest passed

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
@@ -54,10 +54,14 @@ public abstract class FileTransformer implements IFileVisitor {
       writer = decorateWithFilters(writer);
 
       /*
-       * There is a missing piece here: you have an input reader and an ouput writer (notice how the 
+       * There is a missing piece here: you have an input reader and an output writer (notice how the
        * writer has been decorated by the concrete subclass!). You need to write a loop to read the
        * characters and write them to the writer.
        */
+      int b;
+      while ((b = reader.read()) != -1) {
+        writer.write(b);
+      }
       
       reader.close();
       writer.flush();
