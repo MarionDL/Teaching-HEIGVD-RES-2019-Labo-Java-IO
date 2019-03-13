6938df917fd3183b7040a7935f49116a0dec4797
Marion Dutu <marion.dutulaunay@heig-vd.ch>
2019-03-13 16:57:44 +0100

    All tests green

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -7,11 +7,11 @@ import java.util.logging.Logger;
 
 /**
  * This class transforms the streams of character sent to the decorated writer.
- * When filter encounters a line separator, it sends it to the decorated writer.
- * It then sends the line number and a tab character, before resuming the write
+ * When filter encounters a lineNb separator, it sends it to the decorated writer.
+ * It then sends the lineNb number and a tab character, before resuming the write
  * process.
  *
- * Hello\n\World -> 1\Hello\n2\tWorld
+ * Hello\n\World -> 1\tHello\n2\tWorld
  *
  * @author Olivier Liechti
  */
@@ -19,24 +19,64 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
+  // Numeros des lignes
+  private int lineNb = 1;
+  // Detection d'un '\r'
+  private boolean detectR = false;
+
+
   public FileNumberingFilterWriter(Writer out) {
     super(out);
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-    // Ajouter les numeros de ligne
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    this.write(str.toCharArray(), off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    for (int i = off; i < len + off; ++i) {
+      // Appel de la methode definie au-dessous
+      this.write(cbuf[i]);
+    }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    // throw new UnsupportedOperationException("The student has not implemented this method yet.");
 
+    if (lineNb == 1) {
+      this.out.write("1\t");
+      ++lineNb;
+    }
+
+    // Ecrit aussi le \n
+    if (!detectR) {
+      this.out.write(c);
+    }
+    else {
+      if (c != '\n') {
+        this.out.write(lineNb++ + "\t" + (char) c);
+        detectR = false;
+      }
+    }
+
+    switch (c) {
+      case '\n':
+        // On ne l'ecrit que si le r a ete detecte
+        if (detectR) {
+          this.out.write("\n");
+          detectR = false;
+        }
+        this.out.write(lineNb++ + "\t");
+        break;
+      case '\r':
+        detectR = true;
+        break;
+    }
+  }
 }
